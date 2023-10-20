41080171267a35d15a10331b074bc19a1e14d365
21120455 <hoai0455fit@gmail.com>
2023-10-20 16:01:35 +0700

    Design layout Order Page, fake data base on MVVM model.

---
+++ layout-notemplate/Model/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model
+{
+    class Order
+    {
+        public String Product { get; set; }
+        public String OrderId { get; set; }
+
+        public String Date {  get; set; }
+        public String CustomerName{ get; set; }
+        public String Status { get; set; } 
+        public String Amount { get; set; }
+        public static List<Order> Orders()
+        {
+            return new List<Order>(new Order[]
+                {
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"},
+            new Order { Product="Iphone 13 pro",OrderId="#11232",Date="Jun 19, 2022",CustomerName="Ung Chang Im",Status="Deleveried",Amount="$400.00"}
+            });
+
+        }
+
+
+    }
+}
