5dc1255c0dde4efaec71593823aaf6e023656256
21120447 <haonhat2729@gmail.com>
2023-10-25 17:39:52 +0700

    app to index

--- server/controllers/product.Controllers.js
+++ server/controllers/product.Controllers.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   database: process.env.DATABASE,
+  password: process.env.DATABASE_PASSWORD,
 });
 
 cloudinary.config({
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   database: process.env.DATABASE,
+  password: process.env.DATABASE_PASSWORD,
 });
 
 db.connect((err) => {
